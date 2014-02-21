6dfd9d086caf7f9278644688ff5628b44c539cb8
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-21 16:01:33 +0100

    configure GOPATH

--- config.fish
+++ config.fish
@@ -1,4 +1,6 @@
+set -x LD_LIBRARY_PATH /usr/local/lib/
+set -x GOPATH $HOME/go
+
 idempotent_add_to_path /opt/leiningen
 idempotent_add_to_path $HOME/scratch/tools/go/bin
-
-set -x LD_LIBRARY_PATH /usr/local/lib/
+idempotent_add_to_path $GOPATH/bin
