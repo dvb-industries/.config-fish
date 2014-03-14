eb34b499f3dd15e0ac61ad6af42439dc7f34f07f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-03-14 13:05:16 +0100

    add pebble to path

--- config.fish
+++ config.fish
@@ -4,3 +4,4 @@ set -x GOPATH $HOME/go
 idempotent_add_to_path /opt/leiningen
 idempotent_add_to_path $HOME/scratch/tools/go/bin
 idempotent_add_to_path $GOPATH/bin
+idempotent_add_to_path $HOME/scratch/tools/pebble-dev/PebbleSDK-2.0.1/bin
