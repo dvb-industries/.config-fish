4ee55fab35dff3b0843c065e3882f9d344bbf7a7
dvberkel <daan.v.berkel.1980@gmail.com>
2014-09-09 15:25:12 +0200

    bind \ce to forward-char

--- config.fish
+++ config.fish
@@ -1,6 +1,10 @@
 set -x LD_LIBRARY_PATH /usr/local/lib/
 set -x GOPATH $HOME/go
 
+set -x TERM=xterm-256color
+
+bind \ce forward-char
+
 idempotent_add_to_path /opt/leiningen
 idempotent_add_to_path $HOME/scratch/tools/go/bin
 idempotent_add_to_path $GOPATH/bin
